536deb9a048feb077596a8bc1c3e5067b987c283
Jorge Manrique <georgemanc.28@gmail.com>
2025-01-09 16:44:28 -0500

    Proyecto inicial

---
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -0,0 +1,24 @@
+package com.aluracursos.screenmatch.model;
+
+public enum Categoria {
+    ACCIÓN("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIMEN("Crime");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb){
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text){
+        for (Categoria categoria : Categoria.values()){
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)){
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoría encontrada: "  + text);
+    }
+}
